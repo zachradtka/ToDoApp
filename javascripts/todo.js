17b9fd208d66e9822f74aa984be517f55886bf61
Zachary Radtka <zachary.radtka@gmail.com>
2016-09-10 15:57:02 -0400

    Formatted the tasks list with CSS

--- javascripts/todo.js
+++ javascripts/todo.js
@@ -1,21 +1,29 @@
-//TODO: Create a task element with a remove button
+/**
+ * Create a task element.
+ * 
+ * @param  {string} task_value - The text for the task
+ * @return {object} task_element - The task with a remove button
+ */
 function createTask(task_value) {
-
+  // Get a new UUID for the task
   var task_id = uuid();
 
-  // <div class="task"></div>
+  // Construct the outer <div> for the task
   var task_elem = document.createElement("DIV");
   task_elem.setAttribute("class", "task");
   task_elem.setAttribute("id", task_id)
 
-  var task_text = document.createTextNode(task_value);
+  // Create a div for the task text
+  var task_text = document.createElement("DIV");
+  task_text.setAttribute("class", "task_text");
+  var text = document.createTextNode(task_value);
+  task_text.appendChild(text);
   task_elem.appendChild(task_text);
 
-  // <div class="rm_task_btn" onclick="removeTask()">-</div>
+  // Create a button to remove the task 
   var rm_btn = document.createElement("DIV");
   rm_btn.setAttribute("class", "rm_task_btn")
   rm_btn.addEventListener("click", function() { removeTask(task_id) });
-
   var btn_txt = document.createTextNode("-");
   rm_btn.appendChild(btn_txt);
 
@@ -25,6 +33,9 @@ function createTask(task_value) {
   return task_elem;
 }
 
+/**
+ * Add a task to the list of tasks
+ */
 function addTask() {
   var task = document.getElementById("new_task");
 
@@ -35,7 +46,10 @@ function addTask() {
   task.value = "";
 }
 
-
+/**
+ * Remove a task from the list of tasks
+ * @param  {string} task_id - The UUID of the task
+ */
 function removeTask(task_id) {
   var child = document.getElementById(task_id);
   child.parentNode.removeChild(child);
