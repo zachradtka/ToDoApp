b647c98aff77cf719e786f95e830b853b39bb331
Zachary Radtka <zachary.radtka@gmail.com>
2016-09-09 21:23:26 -0400

    Use `createTextNode` instead of `innerHtml`

--- javascripts/todo.js
+++ javascripts/todo.js
@@ -3,11 +3,13 @@
 //TODO: Create a task element with a remove button
 function createTask(task_value) {
   // Create a div 
-  var elem = document.createElement("DIV");
-  elem.setAttribute("class", "task");
-  elem.innerHTML = task_value;
+  var task_text = document.createTextNode(task_value);
 
-  return elem
+  var task_elem = document.createElement("DIV");
+  task_elem.setAttribute("class", "task");
+  task_elem.appendChild(task_text);
+
+  return task_elem;
 }
 
 /**
@@ -18,12 +20,12 @@ function addTask() {
   // Get the task element
   var task = document.getElementById("new_task");
 
-  var task_elem = createTask(task.value)
+  var task_elem = createTask(task.value);
   // Append task to the task list
   document.getElementsByClassName("task_list")[0].appendChild(task_elem);
 
   // Clear the text input value
-  task.value = ""
+  task.value = "";
 }
 
 
