46249d39b999bc5c609e35e37c09f1501c21a004
Zachary Radtka <zachary.radtka@gmail.com>
2016-09-23 19:05:22 -0400

    Used JSHint to lint javascript files

--- javascripts/todo.js
+++ javascripts/todo.js
@@ -15,7 +15,7 @@ function addTask() {
 
   // Clear the text input value
   newTask.value = "";
-  saveLocal()
+  saveLocal();
 }
 
 
@@ -27,7 +27,7 @@ function loadTasks() {
   var taskList = localStorage.getItem("taskList");
   if (taskList) {
 
-    tasks = JSON.parse(taskList)
+    tasks = JSON.parse(taskList);
 
     for (var prop in tasks) {
       if (tasks.hasOwnProperty(prop)) {
@@ -50,9 +50,9 @@ function Task(value, id, createDate, completed, completedDate) {
   this.value = value;
 
   if (id === undefined) {
-    this.uuid = uuid()
+    this.uuid = uuid();
   } else {
-    this.uuid = id
+    this.uuid = id;
   }
 
   if (createDate === undefined) {
@@ -84,7 +84,7 @@ Task.prototype.createHTML = function() {
   // Construct the outer <div> for the task
   var taskElem = document.createElement("DIV");
   taskElem.setAttribute("class", "task");
-  taskElem.setAttribute("id", this.uuid)
+  taskElem.setAttribute("id", this.uuid);
 
   // Add metadata for the task
   taskElem.dataset.createDate = this.createDate;
@@ -92,10 +92,10 @@ Task.prototype.createHTML = function() {
 
   // Create a button to complete the task
   var completeBtn = document.createElement("DIV");
-  completeBtn.setAttribute("class", "complete_task_btn")
+  completeBtn.setAttribute("class", "complete_task_btn");
   completeBtn.addEventListener("click", this.completeTask.bind(this, this.uuid));
-  var btnTxt = document.createTextNode("%");
-  completeBtn.appendChild(btnTxt);
+  var compBtnTxt = document.createTextNode("%");
+  completeBtn.appendChild(compBtnTxt);
   taskElem.appendChild(completeBtn);
 
   // Create a div for the task text
@@ -107,19 +107,19 @@ Task.prototype.createHTML = function() {
 
   // Create a button to remove the task
   var rmBtn = document.createElement("DIV");
-  rmBtn.setAttribute("class", "rm_task_btn")
+  rmBtn.setAttribute("class", "rm_task_btn");
   rmBtn.addEventListener("click", this.removeTask.bind(this, this.uuid));
-  var btnTxt = document.createTextNode("-");
-  rmBtn.appendChild(btnTxt);
+  var rmBtnTxt = document.createTextNode("-");
+  rmBtn.appendChild(rmBtnTxt);
   taskElem.appendChild(rmBtn);
 
   // If the task is complete add the styling for a completed task
   if (this.completed) {
-    taskText.className += " task_complete"
+    taskText.className += " task_complete";
   }
 
   return taskElem;
-}
+};
 
 /**
  * Remove a task from the list of tasks
@@ -131,7 +131,7 @@ Task.prototype.removeTask = function(task_id) {
 
   delete TASKS[task_id];
   saveLocal();
-}
+};
 
 Task.prototype.completeTask = function(task_id) {
 
@@ -153,15 +153,15 @@ Task.prototype.completeTask = function(task_id) {
   for (i = 0; i < children.length; i++) {
     if (children[i].getAttribute("class").includes("task_text")) {
       if (this.completed) {
-        children[i].className = "task_text task_complete"
+        children[i].className = "task_text task_complete";
       } else {
-        children[i].className = "task_text"
+        children[i].className = "task_text";
       }
     }
   }
 
   saveLocal();
-}
+};
 
 /**
  * @return {[type]}
@@ -172,7 +172,7 @@ function uuid() {
     random = Math.random() * 16 | 0;
 
     if (i == 8 || i == 12 || i == 16 || i == 20) {
-      uuid += "-"
+      uuid += "-";
     }
     uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
   }
